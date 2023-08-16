f043d5e5d01cf1370c78e2e3aa19c3890f8a56b0
ysh4me <cmadhkour@myges.fr>
2023-08-16 11:17:02 +0200

    test

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/css/index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import Home from './components/Home';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xs': '370px',
+        'sm': '480px',
+        'md': '768px',
+        'lg': '976px',
+        'xl': '1440px',
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
